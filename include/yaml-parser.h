4ac56946d37f647bc7c325f59a70c2b10283180c
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-06-19 07:58:39 +0300

    add exception header file

--- include/yaml-parser.h
+++ include/yaml-parser.h
@@ -4,5 +4,7 @@
 #include <yaml-parser/token.h>
 #include <yaml-parser/stream.h>
 #include <yaml-parser/decoder.h>
+#include <yaml-parser/element.h>
+#include <yaml-parser/exception.h>
 
 #endif /* YAML_PARSER_H */
---
+++ include/yaml-parser/exception.h
@@ -0,0 +1,8 @@
+#ifndef YAML_PARSER_EXCEPTION_H
+#define YAML_PARSER_EXCEPTION_H
+
+namespace YamlParser
+{
+}
+
+#endif /* YAML_PARSER_EXCEPTION_H */
